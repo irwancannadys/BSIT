0d03d3aa40974234517e95d27ffe816be75cfacf
Irwan Cannady <irwan.cannady@graveltechnology.com>
2023-03-08 13:54:47 +0700

    update adapter and setup viewpager

--- app/src/main/java/com/irwan/bsit/presentation/MainActivity.kt
+++ app/src/main/java/com/irwan/bsit/presentation/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
 import com.irwan.bsit.R
 import com.irwan.bsit.databinding.ActivityMainBinding
 import com.irwan.bsit.model.TransactionResponse
+import com.irwan.bsit.presentation.adapter.BsitFragmentPagerAdapter
 import com.irwan.bsit.presentation.adapter.TransactionAdapter
 import com.irwan.bsit.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,17 +25,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUpViewPager(listOf())
+        setUpTabLayout()
         observeViewModel()
-        mainViewModel.getTransaction()
+//        mainViewModel.getTransaction()
     }
 
-    private fun setUpViewPager(bundle: List<Bundle?>) {
+    private fun setUpViewPager(bundle: List<Bundle>) {
+        val viewPager = binding.vpContainer
+        viewPager.adapter = BsitFragmentPagerAdapter(this, bundle)
+    }
 
+    private fun setUpTabLayout() {
+        val tabLayout = binding.tabsFragment
+        val viewPager = binding.vpContainer
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = "Transaction"
+                1 -> tab.text = "Contact"
+                2 -> tab.text = "Profile"
+            }
+        }.attach()
     }
 
     private fun observeViewModel() {
         mainViewModel.transaction.observe(this) {
-            setDataForTransaction(it)
+//            setDataForTransaction(it)
         }
         mainViewModel.errorMessage.observe(this) {
             Toast.makeText(applicationContext, it, Toast.LENGTH_LONG).show()
@@ -45,17 +62,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setDataForTransaction(data: List<TransactionResponse>) {
         val adapter = TransactionAdapter(data)
-        binding.rvListTransaction.adapter = adapter
+//        binding.rvListTransaction.adapter = adapter
     }
 
     private fun showLoading() {
         binding.cmpLoading.root.visibility = View.VISIBLE
-        binding.rvListTransaction.visibility = View.GONE
+//        binding.rvListTransaction.visibility = View.GONE
     }
 
     private fun hideLoading() {
         binding.cmpLoading.root.visibility = View.GONE
-        binding.rvListTransaction.visibility = View.VISIBLE
+//        binding.rvListTransaction.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/com/irwan/bsit/presentation/adapter/BsitFragmentPagerAdapter.kt
+++ app/src/main/java/com/irwan/bsit/presentation/adapter/BsitFragmentPagerAdapter.kt
@@ -2,17 +2,17 @@ package com.irwan.bsit.presentation.adapter
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.irwan.bsit.presentation.fragment.ContactFragment
 import com.irwan.bsit.presentation.fragment.ProfileFragment
 import com.irwan.bsit.presentation.fragment.TransactionFragment
 
 class BsitFragmentPagerAdapter(
-    fragment: Fragment,
-    private val bundle: List<Bundle>
+    fragment: FragmentActivity,
+    private val bundle: List<Bundle>,
 ) : FragmentStateAdapter(
-    fragment.childFragmentManager,
-    fragment.lifecycle
+    fragment,
 ) {
 
     private val fragment = listOf(
@@ -24,10 +24,10 @@ class BsitFragmentPagerAdapter(
     override fun getItemCount() = fragment.size
 
     override fun createFragment(position: Int): Fragment {
-        val fragmentName = this.fragment.getOrNull(position)
-        val fragmentInstance = fragmentName?.invoke() as? Fragment ?: Fragment()
+        val fragmentName = fragment.getOrNull(position)
+        val fragmentInstance = fragmentName?.invoke() ?: Fragment()
 
-        val bundle = this.bundle.getOrNull(position)
+        val bundle = bundle.getOrNull(position)
         fragmentInstance.arguments = bundle
 
         return fragmentInstance
